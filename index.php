2481ea4da33bc0f6215e51c3179bbda7f13e2456
livramateus <livra.matheus@gmail.com>
2021-10-20 21:19:57 -0300

    Another attempt to fix index file

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once realpath("vendor/autoload.php");
 
+$_GENERAL = [
+    'project_name' => "Matheus's Workout Generator",
+    'author'       => 'Matheus',
+    'since'        => '2020',
+    'project_addr' => 'MatheusWorkoutGenerator'
+];
+
 if (file_exists('env.php')) {
     require_once('env.php');
 }
@@ -9,8 +16,6 @@ use Mwg\Controller\ControllerFooter;
 use Mwg\Core\CookieKeys;
 use Mwg\Core\CookieTerms;
 use Mwg\Core\UrlManager;
-
-// require_once ("src/core/general.php");
 
 new CookieKeys();
 new CookieTerms();
